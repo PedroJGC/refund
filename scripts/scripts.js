a13058c5e4a0148289a24cc7414a143fccc0c511
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-12 21:38:09 -0300

    Format amount input as Brazilian currency

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,25 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById('amount');
 
+// Adiciona um evento de input ao campo de valor
 amount.oninput = () => {
+  // Remove todos os caracteres que não são números
   let value = amount.value.replace(/\D/g, '');
-  amount.value = value;
+
+  // Transforma o valor em centavos (exemplo: 150/100 = 1.50)
+  value = Number(value) / 100;
+
+  // Formata o valor
+  amount.value = formatCurrencyBRL(value);
 };
+
+function formatCurrencyBRL(value) {
+  // Formata o valor para o formato de moeda brasileira
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  // Retorna o valor formatado
+  return value;
+}
